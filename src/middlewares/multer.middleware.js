c08aec14caf609b1b592558db1b5ebe56df1cc76
Akshat-Srivastava2004 <akshatsri2004alld@gmail.com>
2024-05-28 17:47:55 +0530

    newchanges

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,14 +1,24 @@
 import multer from "multer";
+import fs from "fs";
+import path from "path";
+
+// Define the directory path
+const tempDir = path.join(__dirname, "public/temp");
+
+// Ensure the directory exists
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./public/temp")                           // path where all files will stored 
-    },
-    filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
-    }
-  })
-  export const upload = multer({ 
-    storage,
-})
+  destination: function (req, file, cb) {
+    cb(null, tempDir); // path where all files will be stored
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+export const upload = multer({
+  storage,
+});
